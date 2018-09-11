21a2b4d33f1d7c7a4f574f1b858f66856e1c8831
Kanyesigye Edgar <kedgar751@gmail.com>
2018-09-11 08:19:31 +0300

    pass the name using event handler

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Contact = ({ name, email, phone }) => (
-    <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-        </ul>
-    </div>
-);
+class Contact extends React.Component {
+    state = {
+        displayContact: true
+    };
+
+    toggleContactDisplay = (name, event) => {
+        console.log(name); // returns the name right here
+        this.setState({ displayContact: false });
+    };
+
+    render() {
+        const { name, email, phone } = this.props;
+        const { displayContact } = this.state;
+
+        return (
+            <div className="card card-body mb-3">
+                <h4>
+                    {name}
+                    <i
+                        className="fas fa-sort-down"
+                        onClick={this.toggleContactDisplay.bind(this, name)}
+                    />
+                </h4>
+                {displayContact ? (
+                    <ul className="list-group">
+                        <li className="list-group-item">Email: {email}</li>
+                        <li className="list-group-item">Phone: {phone}</li>
+                    </ul>
+                ) : null}
+            </div>
+        );
+    }
+}
 
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
