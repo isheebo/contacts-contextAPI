cf0faeb92a20e5b096470fd32546b28218241861
Kanyesigye Edgar <kedgar751@gmail.com>
2018-10-15 09:06:32 +0300

    prefer HashRouter to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Contacts from './containers/Contacts';
 import Header from './components/layouts/Header';
 import About from './components/pages/About';
 import AddContact from './components/contacts/AddContact';
 import { Provider } from './Context';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/styles.css';
 import NotFound from './components/pages/NotFound';
 import EditContact from './components/contacts/EditContact';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
 
 const App = () => (
     <Provider>
         {/* using browser router may fail on github pages, so we may use
         HashRouter or add the basename to process.env.PUBLIC_URL */}
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <div className="app">
                 <Header branding="Contacts Manager" />
                 <div className="container">
@@ -32,7 +32,7 @@ const App = () => (
                     </Switch>
                 </div>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
 
